5738e50eedc2a1dd1eca26fd446f5b852f6625f5
TRIVEDH <109729596+Trivedh5@users.noreply.github.com>
2022-11-03 15:15:04 +0530

    a,b = map(int,input().split()) p = a*b while a != 0 and b != 0:     if a > b:         a %= b     else:         b %= a if a == 0:     gcd = b else:     gcd = a print(p//gcd)

--- LCM.py
+++ LCM.py
@@ -1,9 +1,12 @@
 a,b = map(int,input().split())
-x = max(a,b)
-i = 1
-while i > 0:
-    m = x * i
-    if m % a == 0 and m % b == 0:
-        print(m)
-        break
-    i += 1
+p = a*b
+while a != 0 and b != 0:
+    if a > b:
+        a %= b
+    else:
+        b %= a
+if a == 0:
+    gcd = b
+else:
+    gcd = a
+print(p//gcd)
